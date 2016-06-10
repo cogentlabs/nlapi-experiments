6f9be9151550383936e5636f821b926472d6c763
philipperemy <premy@reactive.co.jp>
2016-06-10 17:17:44 +0900

    output json

--- attachment-finder/ml-libs/server.py
+++ attachment-finder/ml-libs/server.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import json
+
 from flask import Flask
 from flask import request
 
@@ -13,13 +15,16 @@ app = Flask(__name__)
 def hello():
     ret = 'Hit the endpoint like this http://127.0.0.1:5000?q=TEXT'
     try:
-        sentence = str(request.args['a'])
+        sentence = str(request.args['q'])
         print('Received text = {}'.format(sentence))
         nl_api_element = nl.call_nl_api(sentence)
         print('[NL API] element = {}'.format(nl_api_element))
         predicted_query = logic.build_query(nl_api_element)
         print('[Logic] predicted query = {}'.format(predicted_query))
-        return predicted_query
+
+        out = {'query': predicted_query,
+               'nl_api': nl_api_element}
+        return json.dumps(out)
     except Exception, e:
         print(str(e)), e
     return ret
