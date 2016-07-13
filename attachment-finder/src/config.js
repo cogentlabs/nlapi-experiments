26214cd4c55900be374b22f5d4883ff8252f7589
Ray Sakai <ray.sakai@logical-fabrics.com>
2016-07-13 17:48:20 +0900

    overwriting config

--- attachment-finder/src/config.js
+++ attachment-finder/src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  GOOGLE_API_KEY: 'AIzaSyAehG6BCRqZHL5QdfjiMrixuk79Avj0wdA',
-  QUERY_CONV_ENDPOINT: 'http://gmail-query-builder.reactive.ai/'
+  GOOGLE_API_KEY: '',
+  QUERY_CONV_ENDPOINT: 'http://localhost:5000/'
 }
